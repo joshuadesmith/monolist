82f778a37e537775ccd0cf235f9f3ab91a100705
joshuadesmith <joshuadesmith@utexas.edu>
2018-09-24 15:08:04 -0500

    made sign up styling little less trash
    made the sign in style a little less garbage

--- src/containers/auth/SignUpView.js
+++ src/containers/auth/SignUpView.js
@@ -1,11 +1,13 @@
 import * as React from "react";
-import {View} from "react-native";
+import {Image, StyleSheet, Text, View} from "react-native";
 
 import {connect} from "react-redux";
 import {signUp} from "../../actions";
 
 import {bindActionCreators} from "redux";
 import SignUpForm from "../../forms/SignUpForm";
+
+const lemur = require('../../assets/Wildlife-icons/png/animals-57.png');
 
 class SignUpView extends React.Component {
     constructor(props) {
@@ -23,8 +25,19 @@ class SignUpView extends React.Component {
 
     render() {
         return (
-            <View>
-                <SignUpForm onSubmit={(values) => this.dispatchSignUp(values)}/>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <Image source={lemur} style={styles.headingImage} resizeMode={"contain"}/>
+                </View>
+                <Text style={styles.greeting}>
+                    Welcome!
+                </Text>
+                <Text style={styles.greeting2}>
+                    sign up to continue
+                </Text>
+                <View style={styles.inputContainer}>
+                    <SignUpForm onSubmit={(values) => this.dispatchSignUp(values)}/>
+                </View>
             </View>
         );
     }
@@ -42,3 +55,40 @@ export default connect(
     mapStateToProps,
     mapDispatchToProps
 )(SignUpView);
+
+const styles = StyleSheet.create({
+    modal: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    inputContainer: {
+        marginTop: 20
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 40
+    },
+    greeting: {
+        marginTop: 20,
+        fontSize: 24
+    },
+    greeting2: {
+        color: '#666',
+        fontSize: 24,
+        marginTop: 5
+    },
+    heading: {
+        flexDirection: 'row'
+    },
+    headingImage: {
+        width: 38,
+        height: 38
+    },
+    errorMessage: {
+        fontSize: 12,
+        marginTop: 10,
+        color: 'transparent'
+    }
+});

b8bd5f6ecf46fe66eff9d15d2cd261fb331024aa
joshuadesmith <joshuadesmith@utexas.edu>
2018-09-24 17:38:23 -0500

    renamed root container

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import React from "react";
 import { View } from "react-native";
 import Amplify from "aws-amplify";
 import config from "./aws-amplify-config";
-import RootContainer from "./scenes/RootContainer";
+import RootContainer from "./scenes/RootView";
 
 Amplify.configure(config);
 

93f3be295db5df1aca3c5e849a0ae220739f14ac
joshuadesmith <joshuadesmith@utexas.edu>
2018-09-24 13:37:41 -0500

    added sign in action/saga/reducers

--- src/containers/auth/SignInView.js
+++ src/containers/auth/SignInView.js
@@ -1,23 +1,42 @@
 import * as React from "react";
-import { View, TextInput } from "react-native";
+import {View, TextInput} from "react-native";
 import SignInForm from "../../forms/SignInForm";
+import {bindActionCreators} from "redux";
+import {signIn} from "../../actions";
+import {connect} from "react-redux";
 
 class SignInView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.dispatchSignIn = this.dispatchSignIn.bind(this);
-  }
-  dispatchSignIn(values) {
-    const { email, password } = values;
-    console.log("Username: ", username);
-  }
-  render() {
-    return (
-      <View>
-        <SignInForm onSubmit={values => console.log(values)} />
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.dispatchSignIn = this.dispatchSignIn.bind(this);
+    }
+
+    dispatchSignIn(values) {
+        console.log("Dispatching sign in: ", values);
+        this.props.signIn({
+            email: values.email,
+            password: values.password,
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                <SignInForm onSubmit={values => this.dispatchSignIn(values)}/>
+            </View>
+        );
+    }
 }
 
-export default SignInView;
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+});
+
+const mapDispatchToProps = dispatch => ({
+    signIn: bindActionCreators(signIn, dispatch),
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(SignInView);
--- src/forms/SignInForm.js
+++ src/forms/SignInForm.js
@@ -1,65 +1,66 @@
 import * as React from "react";
-import { reduxForm, Field } from "redux-form";
+import {reduxForm, Field} from "redux-form";
 import {
-  ScrollView,
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet
+    ScrollView,
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet
 } from "react-native";
 
 import CustomTextInput from "../components/CustomTextInput";
 
 const SignInForm = props => {
-  return (
-    <View style={styles.container}>
-      <ScrollView keyboardShouldPersistTaps={"handled"}>
-        <Field
-          name={"username"}
-          component={CustomTextInput}
-          placeholder={"Username"}
-          style={styles.textField}
-        />
-        <Field
-          name={"password"}
-          component={CustomTextInput}
-          placeholder={"Password"}
-          style={styles.textField}
-        />
-        <TouchableOpacity onPress={props.handleSubmit}>
-          <Text>Sign In</Text>
-        </TouchableOpacity>
-      </ScrollView>
-    </View>
-  );
+    return (
+        <View>
+            <ScrollView keyboardShouldPersistTaps={"handled"}>
+                <Field
+                    name={"email"}
+                    component={CustomTextInput}
+                    placeholder={"Email"}
+                    style={styles.textField}
+                />
+                <Field
+                    name={"password"}
+                    component={CustomTextInput}
+                    placeholder={"Password"}
+                    style={styles.textField}
+                    secureTextEntry
+                />
+                <TouchableOpacity onPress={props.handleSubmit}>
+                    <Text>Sign In</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </View>
+    );
 };
 
-export default reduxForm({ form: "signInForm" })(SignInForm);
+export default reduxForm({form: "signInForm"})(SignInForm);
 
 const styles = StyleSheet.create({
-  container: {
-    margin: 15,
-    padding: 10,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#d1d1d1"
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: "center",
-    margin: 10
-  },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
-  },
-  textField: {
-    color: "#000000",
-    backgroundColor: "#ffffff",
-    marginHorizontal: 10,
-    marginBottom: 10,
-    paddingVertical: 10,
-    fontSize: 20
-  }
+    container: {
+        margin: 15,
+        padding: 10,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#d1d1d1"
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10
+    },
+    instructions: {
+        textAlign: "center",
+        color: "#333333",
+        marginBottom: 5
+    },
+    textField: {
+        color: "#000000",
+        backgroundColor: "#ffffff",
+        marginHorizontal: 10,
+        marginBottom: 10,
+        paddingVertical: 10,
+        fontSize: 20
+    }
 });

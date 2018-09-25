550177b38722d5bbfc165b64427e480381c39e13
joshuadesmith <joshuadesmith@utexas.edu>
2018-09-25 17:30:29 -0500

    added components for linking services

--- src/scenes/user/settings/SettingsView.js
+++ src/scenes/user/settings/SettingsView.js
@@ -1,35 +1,9 @@
 import React from "react";
-import {View, StyleSheet, Text, Image} from "react-native";
-import {colors} from "../../../styles";
+import {View, Text, Image} from "react-native";
+import styles from "./styles";
+import LinkServicesView from "./link/LinkServicesView";
 
 const backImg = require("../../../assets/icons/baseline_home_black_48dp.png");
-
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#666666',
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-        flex: 1
-    },
-    section: {
-        paddingTop: 15,
-        paddingLeft: 15,
-    },
-    settingsHeader: {
-        color: colors.lightGrey,
-        marginBottom: 26,
-        fontSize: 20,
-        textAlign: 'center'
-    },
-    backImage: {
-        tintColor: colors.lightGrey,
-        height: 30,
-        width: 30,
-        marginLeft: 10,
-        marginRight: 5,
-        marginBottom: 5,
-    }
-});
 
 class SettingsView extends React.Component {
     static navigationOptions = {
@@ -40,11 +14,7 @@ class SettingsView extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.section}>
-                    <Text style={styles.settingsHeader}>
-                        Link Services
-                    </Text>
-                </View>
+                <LinkServicesView/>
             </View>
         )
     }

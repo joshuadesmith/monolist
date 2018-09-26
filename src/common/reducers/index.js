8ec6fad2c385f1a5e2052096a2ae0c40988df5f5
joshuadesmith <joshuadesmith@utexas.edu>
2018-09-26 12:17:39 -0500

    added reducers for requesting spotify auth code; updated gitignore

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,19 +1,21 @@
-import { combineReducers } from "redux";
-import { reducer as form } from "redux-form";
+import {combineReducers} from "redux";
+import {reducer as form} from "redux-form";
 import authReducer from "../../scenes/auth/reducers/auth";
+import settingsReducer from "../../scenes/user/settings/reducers";
 
 export const RESET_GLOBAL_STATE = "RESET_GLOBAL_STATE";
 
 const appReducer = combineReducers({
-  form: form,
-  auth: authReducer
+    form: form,
+    auth: authReducer,
+    settings: settingsReducer,
 });
 
 const rootReducer = (state, action) => {
-  if (action.type === RESET_GLOBAL_STATE) {
-    state = undefined;
-  }
-  return appReducer(state, action);
+    if (action.type === RESET_GLOBAL_STATE) {
+        state = undefined;
+    }
+    return appReducer(state, action);
 };
 
 export default rootReducer;
